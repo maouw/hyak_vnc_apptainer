aa8c2b160be958585c6198ac484392d6a5ded767
Altan Orhon <altan@uw.edu>
2024-01-15 21:09:32 -0800

    update

--- common/scripts/setup-ubuntu22.04-base.sh
+++ common/scripts/setup-ubuntu22.04-base.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-set -ex
 
-export DEBIAN_FRONTEND=noninteractive
-# Don't generate these locales"
+# This script is designed to be sourced by other scripts, not run directly.
+
+# Don't generate these locales:
 mkdir -p /var/lib/locales/supported.d
 for localename in fr de zh-hant ja es it zh-hans pt ru ar; do
 	touch "/var/lib/locales/supported.d/${localename}" && chmod a-r "/var/lib/locales/supported.d/${localename}"
@@ -11,14 +11,14 @@ done
 # Limit the English locales to en_US.UTF-8:
 echo "en_US.UTF-8 UTF-8" >/var/lib/locales/supported.d/en && chmod a-r /var/lib/locales/supported.d/en
 
-# Generate the locales
-apt-get install -y locales
-localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.utf8
+apt-get install -yq locales
+locale-gen en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 LC_COLLATE=C
 
 # Set the locale environment variables permanently:
-export LANG="${LANG:-en_US.utf8}" && echo "LANG=\"${LANG}\"" >>/etc/environment
-export LC_ALL="${LANG:-en_US.utf8}" && echo "LC_ALL=\"${LC_ALL}\"" >>/etc/environment
-export LC_COLLATE="${LC_COLLATE:-C}" && echo "LC_COLLATE=\"${LC_COLLATE}\"" >>/etc/environment
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_COLLATE="C"
 
 # Install basic packages:
 apt-get install -yq --no-install-recommends \
--- common/scripts/setup-ubuntu22.04-hyakvnc-vncserver.sh
+++ common/scripts/setup-ubuntu22.04-hyakvnc-vncserver.sh
@@ -1,27 +1,68 @@
 #! /bin/sh
-set -ex
 
+# This script is designed to be sourced by other scripts, not run directly.
 export DEBIAN_FRONTEND=noninteractive
+
+# Don't generate these locales:
+mkdir -p /var/lib/locales/supported.d
+for localename in fr de zh-hant ja es it zh-hans pt ru ar; do
+	touch "/var/lib/locales/supported.d/${localename}" && chmod a-r "/var/lib/locales/supported.d/${localename}"
+done
+
+# Limit the English locales to en_US.UTF-8:
+echo "en_US.UTF-8 UTF-8" >/var/lib/locales/supported.d/en && chmod a-r /var/lib/locales/supported.d/en
+
+apt-get install -yq locales
+locale-gen en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8 LC_COLLATE=C
+
+# Set the locale environment variables permanently:
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_COLLATE="C"
+
+# Install basic packages:
+apt-get install -yq --no-install-recommends \
+	bash-completion \
+	bc \
+	binutils \
+	bzip2 \
+	curl \
+	dash \
+	ed \
+	file \
+	git \
+	less \
+	make \
+	nano \
+	netcat-openbsd \
+	openssh-client \
+	patch \
+	procps \
+	psmisc \
+	python3 \
+	rsync \
+	unzip \
+	vim-tiny \
+	xz-utils \
+	zip
 
 # Stop apt from installing audio packages (they don't work on VNC):
 apt-mark hold xfce4-pulseaudio-plugin pavucontrol pulseaudio pipewire pipewire-bin pipewire-pulse
 
 # Install XFCE and some apps:
 apt-get install -yq --no-install-recommends \
-	at-spi2-core \
 	dbus-x11 \
 	elementary-xfce-icon-theme \
 	fonts-dejavu-core \
 	fonts-ubuntu \
 	gettext \
-	greybird-gtk-theme \
 	libfile-mimeinfo-perl \
 	libnet-dbus-perl \
 	libnotify-bin \
 	libx11-protocol-perl \
 	libxfce4ui-utils \
 	mousepad \
-	novnc \
 	ristretto \
 	thunar \
 	thunar-archive-plugin \
@@ -35,10 +76,8 @@ apt-get install -yq --no-install-recommends \
 	xfce4-appfinder \
 	xfce4-datetime-plugin \
 	xfce4-indicator-plugin \
-	xfce4-notes-plugin \
 	xfce4-notifyd \
 	xfce4-panel \
-	xfce4-screenshooter \
 	xfce4-session \
 	xfce4-settings \
 	xfce4-taskmanager \
@@ -48,8 +87,7 @@ apt-get install -yq --no-install-recommends \
 	xfdesktop4 \
 	xfonts-base \
 	xfwm4 \
-	xterm \
-	zenity
+	xterm
 
 # Set xfce4-terminal as default terminal emulator:
 update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper
@@ -64,4 +102,5 @@ echo '$vncUserDir = "$ENV{VNC_USER_DIR}";' >>/etc/turbovncserver.conf
 cp /opt/setup/scripts/hyakvnc-vncserver.sh /usr/local/bin/hyakvnc-vncserver && chmod a+x /usr/local/bin/hyakvnc-vncserver
 
 # Copy configs:
-rsync -avi --no-p --no-g --chmod=ugo=rwX /opt/setup/configs/ubuntu/ / || { echo "warning: rsync failed!" >&2; exit 1; }
+( cd /opt/setup/configs/ubuntu && cp -rd --parents etc /etc && cp -rd --parents usr /usr ) || { echo "warning: copying configs failed!" >&2; exit 1; }
+
